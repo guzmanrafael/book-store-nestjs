b0be442864e2a1731fec509516e213c905d0022e
guzmanrafael <rafael.guzman1609@gmail.com>
2021-06-04 11:26:10 -0700

    initial commit

---
+++ src/database/migrations/1622830066744-fix_name_detail.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class fixNameDetail1622830066744 implements MigrationInterface {
+    name = 'fixNameDetail1622830066744'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "name" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "lastName" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "created_at" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "updated_at" DROP NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "updated_at" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "created_at" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "lastName" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user_details" ALTER COLUMN "name" SET NOT NULL`);
+    }
+
+}
